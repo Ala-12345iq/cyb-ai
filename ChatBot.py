af175e0d50ae77da6f3c8aaff367f0e63076395a
Ala-12345iq <aghafawwaz14@gmail.com>
2026-06-15 17:35:47 +0700

    Update ChatBot.py

--- ChatBot.py
+++ ChatBot.py
@@ -33,7 +33,7 @@ API_KEY = os.getenv("MY_API_KEY")
 try:
     genai.configure(api_key=API_KEY)
     model = genai.GenerativeModel(
-        model_name="gemini-1.5-flash",
+        model_name="models/gemini-1.5-flash",
         system_instruction="Kamu adalah Tech-Guard AI. Hanya bantu soal teknologi. Tolak topik lain dengan sopan."
     )
     
